d1d24ce5793bb0bf3bff99c5c6c17c396c5c4936
mld-njupt <you@1761311608@qq.com>
2022-06-21 10:47:56 +0800

    fix(eslintrc): 修改eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,13 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'react-app',
+    'shared-config',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
