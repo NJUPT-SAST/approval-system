ffe6aa1866de82fd9d618d35086a37917158cef1
MaxtuneLee <max@xox.im>
2022-07-10 00:10:48 +0800

    feat(Activity): 1.全局的TopBar组件 2.全局的背景色改为灰色 3.活动广场的搜索栏

--- src/pages/activity/index.tsx
+++ src/pages/activity/index.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { RollbackOutlined, SearchOutlined } from '@ant-design/icons'
+import { Breadcrumb, Button } from 'antd'
+import Input from 'antd/lib/input'
+import React, { Fragment } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import TopBar from '../../components/TopBar'
+import './index.scss'
+
+const { Search } = Input
 
 function Activity() {
-  return <div>Activity</div>
+  const onSearch = () => {
+    console.log('searching')
+  }
+  return (
+    <div className="activity">
+      <TopBar />
+      <div className="searchBody">
+        <Search placeholder="搜索比赛活动或关键词" className="searchBar" onSearch={onSearch} enterButton={`搜索`} />
+      </div>
+      <div className="filter"></div>
+      <div className="activitiesBody"></div>
+    </div>
+  )
 }
 
 export default Activity
