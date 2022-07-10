840b661d6862e2c94da0bf3c5ef22fe8102b1237
yanghh2103 <506446818@qq.com>
2022-07-10 22:28:30 +0800

    fix: reviewed切图

--- src/pages/review/index.tsx
+++ src/pages/review/index.tsx
@@ -1,7 +1,164 @@
 import React from 'react'
+import TopBar from '../../components/TopBar'
+import { Space, Button, Table } from 'antd'
+import type { ColumnsType } from 'antd/lib/table'
+import { Link } from 'react-router-dom'
+import './index.scss'
+
+interface DataType {
+  id: number
+  name: string
+  notreviewed: number
+  reviewed: number
+  start_date: string
+  end_date: string
+}
+const columns: ColumnsType<DataType> = [
+  {
+    title: '序号',
+    dataIndex: 'id',
+  },
+  {
+    title: '比赛名称',
+    dataIndex: 'name',
+  },
+  {
+    title: '待评审数',
+    dataIndex: 'notreviewed',
+  },
+  {
+    title: '已评审数',
+    dataIndex: 'reviewed',
+  },
+  {
+    title: '评审开始日期',
+    dataIndex: 'start_date',
+  },
+  {
+    title: '评审截止日期',
+    dataIndex: 'end_date',
+  },
+  {
+    title: '操作',
+    render: () => (
+      // render 返回一个组件
+      <Space size="middle">
+        <Link to={`/review/${'"挑战杯"创新创业大赛'}`}>
+          <Button className="count" type="primary" key="1">
+            审批
+          </Button>
+        </Link>
+      </Space>
+    ),
+  },
+]
+const data: DataType[] = [
+  {
+    id: 1,
+    name: 'yang',
+    notreviewed: 4,
+    reviewed: 2,
+    start_date: '2022年7月10日',
+    end_date: '2022年7月20日',
+  },
+  {
+    id: 2,
+    name: 'yang',
+    notreviewed: 4,
+    reviewed: 2,
+    start_date: '2022年7月10日',
+    end_date: '2022年7月20日',
+  },
+  {
+    id: 3,
+    name: 'yang',
+    notreviewed: 4,
+    reviewed: 2,
+    start_date: '2022年7月10日',
+    end_date: '2022年7月20日',
+  },
+  {
+    id: 4,
+    name: 'yang',
+    notreviewed: 4,
+    reviewed: 2,
+    start_date: '2022年7月10日',
+    end_date: '2022年7月20日',
+  },
+  {
+    id: 5,
+    name: 'yang',
+    notreviewed: 4,
+    reviewed: 2,
+    start_date: '2022年7月10日',
+    end_date: '2022年7月20日',
+  },
+  {
+    id: 6,
+    name: 'yang',
+    notreviewed: 4,
+    reviewed: 2,
+    start_date: '2022年7月10日',
+    end_date: '2022年7月20日',
+  },
+  {
+    id: 7,
+    name: 'yang',
+    notreviewed: 4,
+    reviewed: 2,
+    start_date: '2022年7月10日',
+    end_date: '2022年7月20日',
+  },
+  {
+    id: 8,
+    name: 'yang',
+    notreviewed: 4,
+    reviewed: 2,
+    start_date: '2022年7月10日',
+    end_date: '2022年7月20日',
+  },
+  {
+    id: 9,
+    name: 'yang',
+    notreviewed: 4,
+    reviewed: 2,
+    start_date: '2022年7月10日',
+    end_date: '2022年7月20日',
+  },
+  {
+    id: 10,
+    name: 'yang',
+    notreviewed: 4,
+    reviewed: 2,
+    start_date: '2022年7月10日',
+    end_date: '2022年7月20日',
+  },
+  {
+    id: 11,
+    name: 'yang',
+    notreviewed: 4,
+    reviewed: 2,
+    start_date: '2022年7月10日',
+    end_date: '2022年7月20日',
+  },
+]
 
 function Review() {
-  return <div>Review</div>
+  return (
+    <div className="manage">
+      <TopBar />
+      <div className="manage-content">
+        <div className="manage-content-table">
+          <div>
+            <h1 className="manage-content-table-title">活动评审</h1>
+            <div className="manage-content-table-body">
+              <Table columns={columns} dataSource={data} pagination={{ pageSize: 9 }} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Review
