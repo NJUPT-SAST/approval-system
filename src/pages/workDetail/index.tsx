4bc3d54f016dc13e893400923cfd6176fae10343
MaxtuneLee <max@xox.im>
2022-08-30 12:49:10 +0800

    feat(workDetail): 文件上传

--- src/pages/workDetail/index.tsx
+++ src/pages/workDetail/index.tsx
@@ -1,4 +1,4 @@
-import { Button, message } from 'antd'
+import { Button, message, UploadProps } from 'antd'
 import FormRender, { useForm } from 'form-render'
 import React, { Fragment, useLayoutEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
@@ -55,7 +55,7 @@ function WorkDetail() {
     useLayoutEffect(() => {
       setLoading(true)
       getCompetitionInfo(Number(id)).then((res) => {
-        console.log(res)
+        // console.log(res)
         setCompetitionDetail(res.data.data)
         setTimeout(() => {
           setLoading(false)
@@ -70,15 +70,77 @@ function WorkDetail() {
     console.log('formData:', formData, 'errors', errors)
   }
 
+  const uploadFileOnchange: UploadProps['onChange'] = (info: any) => {
+    console.log(info)
+  }
+
+  const customUpload = (options: any) => {
+    console.log(options)
+    const { onSuccess, onError, file, onProgress } = options
+    const formData = new FormData()
+    if (id !== undefined) {
+      formData.append('id', id.toString())
+    }
+    formData.append('input', file)
+  }
+
   const schema: any = useGetWorkSchema()
-  console.log(schema)
+  const localSchema: any = {
+    type: 'object',
+    labelWidth: 120,
+    properties: {
+      isstitp: {
+        enum: ['a', 'b'],
+        type: 'string',
+        title: '是否为STITP项目',
+        widget: 'radio',
+        required: true,
+        enumNames: ['是', '否'],
+      },
+      upload_1: {
+        type: 'string:upload',
+        title: '申报书',
+        required: true,
+        props: {
+          maxCount: '1',
+          onChange(info: any) {
+            console.log(info)
+          },
+          customRequest(options: any) {
+            console.log(options)
+            const { onSuccess, onError, file, onProgress } = options
+            const formData = new FormData()
+            if (id !== undefined) {
+              formData.append('id', id.toString())
+            }
+            formData.append('input', file)
+          },
+          headers: { Token: localStorage.getItem('token') },
+        },
+      },
+      upload_2: { type: 'string:upload', title: '研究报告', required: true },
+      upload_3: { type: 'string:upload', title: '作品简介书', required: true },
+      workname: { type: 'string', props: {}, title: '作品名称', required: true },
+      worktype: {
+        enum: ['a', 'b', 'c', 'd'],
+        type: 'string',
+        title: '作品类别',
+        widget: 'select',
+        required: true,
+        enumNames: ['自然科学类学术论文', '哲学社会科学类社会调查报告和学术论文', '科技发明制作A类', '科技发明制作B类'],
+      },
+      workintro: { type: 'string', props: {}, title: '作品简介', format: 'textarea', required: true },
+    },
+    displayType: 'column',
+  }
+  // console.log(schema)
   return (
     <div>
       <TopBar activity={competitionDetail.name} />
       <div className="work-detail-body">
         <div className="title">作品提交信息</div>
         <div className="work-detail-box">
-          {schema !== undefined ? <FormRender form={form} schema={schema.data} onFinish={submitData} /> : <></>}
+          {schema !== undefined ? <FormRender debug form={form} schema={localSchema} onFinish={submitData} /> : <></>}
           <Button type="primary" onClick={form.submit}>
             提 交
           </Button>
