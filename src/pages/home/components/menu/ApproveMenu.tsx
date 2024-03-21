13badbd80c403bf011c66750b866f971e4048e25
Love98 <liaocai@sast.fun>
2024-03-21 16:13:28 +0800

    fix: icon error on menu

--- src/pages/home/components/menu/ApproveMenu.tsx
+++ src/pages/home/components/menu/ApproveMenu.tsx
@@ -1,4 +1,4 @@
-import { AuditOutlined, DashboardOutlined, MailOutlined, UserOutlined } from '@ant-design/icons'
+import { DashboardOutlined } from '@ant-design/icons'
 import { useRecoilValue } from 'recoil'
 import { userInboxPointState } from '../../../../store/userInboxState'
 import { Menu } from 'antd'
@@ -13,16 +13,16 @@ function ApproveMenu(props: any) {
       selectedKeys={[props.navigation]}
       defaultSelectedKeys={['/']}
     >
-      <Menu.Item key="/" icon={<UserOutlined />}>
+      <Menu.Item key="/" icon={<DashboardOutlined />}>
         我的账号
       </Menu.Item>
-      <Menu.Item key="/inbox" icon={<MailOutlined />}>
+      <Menu.Item key="/inbox" icon={<DashboardOutlined />}>
         收件箱{point === 'on' ? <span className="message-read-or-not"></span> : <></>}
       </Menu.Item>
       <Menu.Item key="/activity" icon={<DashboardOutlined />}>
         活动广场
       </Menu.Item>
-      <Menu.Item key="/review" icon={<AuditOutlined />}>
+      <Menu.Item key="/review" icon={<DashboardOutlined />}>
         活动评审
       </Menu.Item>
     </Menu>
--- src/pages/home/components/menu/UserMenu.tsx
+++ src/pages/home/components/menu/UserMenu.tsx
@@ -1,4 +1,4 @@
-import { DashboardOutlined, MailOutlined, UserOutlined } from '@ant-design/icons'
+import { DashboardOutlined } from '@ant-design/icons'
 import { useRecoilValue } from 'recoil'
 import { userInboxPointState } from '../../../../store/userInboxState'
 import { Menu } from 'antd'
@@ -13,10 +13,10 @@ function UserMenu(props: any) {
       selectedKeys={[props.navigation]}
       defaultSelectedKeys={['/']}
     >
-      <Menu.Item key="/" icon={<UserOutlined />}>
+      <Menu.Item key="/" icon={<DashboardOutlined />}>
         我的账号
       </Menu.Item>
-      <Menu.Item key="/inbox" icon={<MailOutlined />}>
+      <Menu.Item key="/inbox" icon={<DashboardOutlined />}>
         收件箱{point === 'on' ? <span className="message-read-or-not"></span> : <></>}
       </Menu.Item>
       <Menu.Item key="/activity" icon={<DashboardOutlined />}>
